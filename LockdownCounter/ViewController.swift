aa91bc99918e4b6b474578e8ac6d1693cac7063c
Sam Miller <sammybobmiller1@gmail.com>
2020-03-30 18:30:57 +0100

    Initial Commit

---
+++ LockdownCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LockdownCounter
+//
+//  Created by Samuel Miller on 30/03/2020.
+//  Copyright © 2020 RHM Computing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
